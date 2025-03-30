6548322295b05c47df4a7b87c8f6c63eef089eec
Aleksey <mr.ryabov.aa@yandex.ru>
2025-03-30 10:47:52 +0300

    Add learning project SpringBoot

---
+++ SpringBoot/src/test/java/ru/ryaboman/learning/spring_boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ryaboman.learning.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
