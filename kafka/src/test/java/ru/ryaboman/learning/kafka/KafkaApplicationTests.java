0c871c2f37e099d544ecb3226c84c4bd6682afc1
Aleksey <mr.ryabov.aa@yandex.ru>
2025-03-30 10:56:03 +0300

    Add files projects

---
+++ kafka/src/test/java/ru/ryaboman/learning/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ryaboman.learning.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
