c5936f11540190495f8f0b1499c4148b3660844b
Aleksey <mr.ryabov.aa@yandex.ru>
2025-04-14 22:49:32 +0300

    Добавляет установку количества посещений страницы в сессию JavaEE

--- JakartaEETest/src/main/java/ru/ryaboman/projects/jakartaeetest/HelloServlet.java
+++ JakartaEETest/src/main/java/ru/ryaboman/projects/jakartaeetest/HelloServlet.java
@@ -13,12 +13,21 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        HttpSession session = request.getSession();
+        Integer count = (Integer) session.getAttribute("count");
+        if (count == null) {
+            count = 0;
+        }
+        session.setAttribute("count", ++count );
+
         response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("<p>" + "Вы посетили страницу " + count + " раз." + "</p>");
         out.println("</body></html>");
     }
 
